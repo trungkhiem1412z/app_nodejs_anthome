cf83fabb9eef70f9a1fdcc9e955fcb4113972298
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-02-28 23:35:29 +0700

    create base

---
+++ config/connectDB.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize');
+
+// asm-angular
+const sequelize = new Sequelize('ant_home', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
+module.exports = sequelize;
