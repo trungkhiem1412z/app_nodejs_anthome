ba4049e7d8ca76513e65efc0cf2dc2ff7af2f78a
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-03-10 19:39:09 +0700

    update

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const routes = require("./routes/routes");
-const cors = require("cors");
+const express = require('express');
+const routes = require('./routes/routes');
+const cors = require('cors');
 
 // .ENV
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
 
@@ -15,14 +15,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // routes
-app.use("/", routes);
+app.use('/', routes);
 
 // error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send("Something broke!");
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 // start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
